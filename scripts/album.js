40436ba771e2cc894432abc3f0c96019a18d922d
Thuy Pham <tpham045@hotmail.com>
2016-11-07 23:33:01 +0000

    Refactor album.js and collection.js with jQuery

--- scripts/album.js
+++ scripts/album.js
@@ -56,29 +56,30 @@ var createSongRow = function(songNumber, songName, songLength) {
     '  <td class="song-item-duration">' + songLength + '</td>' +
     '</tr>';
 
-  return template;
+  return $(template);
 };
 
 var setCurrentAlbum = function(album) {
   //Get album element
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+  var $albumTitle = $('.album-view-title');
+  var $albumArtist = $('.album-view-artist');
+  var $albumReleaseInfo = $('.album-view-release-info');
+  var $albumImage = $('.album-cover-art');
+  var $albumSongList = $('.album-view-song-list');
 
   //Assign album information
-  albumTitle.firstChild.nodeValue = album.title;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
+  $albumTitle.text(album.title);
+  $albumArtist.text(album.artist);
+  $albumReleaseInfo.text(album.year + ' ' + album.label);
+  $albumImage.attr('src', album.albumArtUrl);
 
   //Clean album view first
-  albumSongList.innerHTML = '';
+  $albumSongList.empty();
 
   //Then loop through each song of an album
   for (var i = 0; i < album.songs.length; i++) {
-    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    $albumSongList.append($newRow);
   }
 };
 
@@ -100,7 +101,7 @@ window.onload = function() {
       // Change the content from the number to the play button's HTML
       var songItem = getSongItem(event.target);
       var songItemNumber = songItem.getAttribute('data-song-number');
-      
+
       if (songItemNumber !== currentlyPlayingSong) {
         songItem.innerHTML = playButtonTemplate;
       }
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,26 +1,30 @@
-var collectionItemTemplate =
-  '<div class="collection-album-container column fourth">' +
-  '  <img src="assets/images/album_covers/01.png"/>' +
-  '  <div class="collection-album-info caption">' +
-  '    <p>' +
-  '      <a class="album-name" href="album.html"> The Colors </a>' +
-  '      <br/>' +
-  '      <a href="album.html"> Pablo Picasso </a>' +
-  '      <br/>' +
-  '      X songs' +
-  '      <br/>' +
-  '    </p>' +
-  '  </div>' +
-  '</div>';
+ var buildCollectionItemTemplate = function() {
+   var template =
+     '<div class="collection-album-container column fourth">' +
+     '  <img src="assets/images/album_covers/01.png"/>' +
+     '  <div class="collection-album-info caption">' +
+     '    <p>' +
+     '      <a class="album-name" href="album.html"> The Colors </a>' +
+     '      <br/>' +
+     '      <a href="album.html"> Pablo Picasso </a>' +
+     '      <br/>' +
+     '      X songs' +
+     '      <br/>' +
+     '    </p>' +
+     '  </div>' +
+     '</div>';
+   return $(template);
+ };
+ $(window).load(function() {
+   // Select the whole section album covers
+   var $collectionContainer = $('.album-covers');
+   // Clean the container first
+   $collectionContainer.empty();
 
-window.onload = function() {
-  // Select the whole section album covers
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
-  // Clean the container first
-  collectionContainer.innerHTML = '';
-
-  // Then display the template for each of 13 albums
-  for (var i = 0; i < 12; i++) {
-    collectionContainer.innerHTML += collectionItemTemplate;
-  }
-}
+   // Then display the template for each of 13 albums
+   for (var i = 0; i < 12; i++) {
+     var $newThumbnail = buildCollectionItemTemplate();
+     $collectionContainer.append($newThumbnail);
+   }
+ });
+ 
