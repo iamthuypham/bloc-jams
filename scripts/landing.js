1499e829ecb5efb7d020480a96555eb89eba03eb
Thuy Pham <tpham045@hotmail.com>
2016-11-07 22:54:05 +0000

    Refactor landing.js with jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
+var animatePoints = function() {
   //Function to reveal each elements of sellingpoint
   var revealPoint = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    // #7
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
   };
-  revealPoint();
+  $.each($('.point'), revealPoint);
 };
 //Event handler executes when browser has loaded
-window.onload = function() {
+$(window).load(function() {
   //Immediately load animation for device with tall screen
-  if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+  if ($(window).height() > 950) {
+    animatePoints();
+  }
   //Otherwise load animation based on scrolling event
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener('scroll', function(event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-         animatePoints(pointsArray);   
-     }
-  });
-}
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
+    }
+  })
+});
+ 
