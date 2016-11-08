2f40d5bf4e44b30ecaad70b4c1be76fde07c5298
Thuy Pham <tpham045@hotmail.com>
2016-11-08 05:42:01 +0000

    nextSong() bloc

--- scripts/album.js
+++ scripts/album.js
@@ -93,26 +93,36 @@ var trackIndex = function(album, song) {
   return album.songs.indexOf(song);
 };
 
-var previousSongIndex = null
-var previousSongFromAlbum = null
+var nextSong = function(album, song) {
+  var getLastSongNumber = function(index) {
+    return index == 0 ? currentAlbum.songs.length : index;
+  };
 
-var nextSong = function(album, song){
-  var currentSongIndex = trackIndex(album,currentSongFromAlbum)
-  previousSongIndex = currentSongIndex;
-  var previousyPlayingSongElement = $('.song-item-number[data-song-number="' + previousSongIndex + 1 + '"]');
-  if (currentSongIndex === album.songs.length - 1){
+  var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+  // Note that we're _incrementing_ the song here
+  currentSongIndex++;
+
+  if (currentSongIndex >= currentAlbum.songs.length) {
     currentSongIndex = 0;
-  } else {
-    currentSongIndex += 1
   }
-  currentSongFromAlbum = album.songs[currentSongIndex];
-  updatePlayerBarSong();
+
+  // Set a new current song
   currentlyPlayingSongNumber = currentSongIndex + 1;
-  
-  var songNumber = currentSongFromAlbum.find('.song-item-number');
-  songNumber.html(pauseButtonTemplate)
-  previousyPlayingSongElement = previousSongIndex + 1
-  
+  currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+  // Update the Player Bar information
+  $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+  $('.currently-playing .artist-name').text(currentAlbum.artist);
+  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
+  $('.main-controls .play-pause').html(playerBarPauseButton);
+
+  var lastSongNumber = getLastSongNumber(currentSongIndex);
+  var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+  var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+  $nextSongNumberCell.html(pauseButtonTemplate);
+  $lastSongNumberCell.html(lastSongNumber);
+
 };
 
 //Sycn Pause button on album list and play controll bar
